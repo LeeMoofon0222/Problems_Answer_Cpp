77c193cc2e676ed0d451fbca01213248e7dd94eb
LeeMoofon0222 <moofon0222@gmail.com>
2023-12-04 23:02:57 +0800

    1650

---
+++ Uva/1650.cpp
@@ -0,0 +1,30 @@
+#include<stdio.h>
+using namespace std;
+int main(){
+    double  x,a,b,n,Min,n1,n2;
+
+    scanf("%lf%lf",&x,&n);
+    a=1;
+    b=1;
+    Min=n+1;
+    while(a<=n&&b<=n){
+        if(a/b>x){
+            if(a/b-x<Min){
+                Min=a/b-x;
+                n1=a;
+                n2=b;
+            }
+            b++;
+        }
+        else{
+            if(x-a/b<Min){
+                Min=x-a/b;
+                n1=a;
+                n2=b;
+            }
+            a++;
+        }
+    }
+    printf("%.0f %.0f",n1,n2);
+    return 0;
+}
