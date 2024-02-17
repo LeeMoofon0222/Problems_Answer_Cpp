805c30de4a4bf4382a26f18e71b4bd0fa7c33b00
LeeMoofon0222 <moofon0222@gmail.com>
2024-02-18 02:39:08 +0800

    That's the true 2106

--- POJ/2106.cpp
+++ POJ/2106.cpp
@@ -1,105 +1,93 @@
 //容器適配器 stack 與 vector
+// 要把判斷是否為empty放在前面，否則無法運作
 #include<iostream>
-#include<vector>
-#include <string>
+#include<stack>
+#include<stdio.h>
 using namespace std;
 
-vector<char> v;
-vector<char> nv;
+stack<int> op, val, s1;
 
-
-
-
-char Not(char c1){
-    if(c1=='V'){
-        return 'F';
+void insertVal(int b,int d){
+    while(!op.empty() && op.top()==3){
+        b=!b;
+        op.pop();
     }
-    return 'V';
+    if(d) val.pop();
+    val.push(b);
 }
-char And(char c1,char c2){
-    if(c1=='V' && c2=='V'){
 
-        return 'V';
+void calc(){
+    int a = val.top();
+    val.pop();
+    int b = val.top();
+    val.pop();
+    int c = (a && b);
+    int opr = op.top();
+    op.pop();
+    if(opr == 1){
+        c = (a || b);
     }
-    return 'F';
+    insertVal(c,0);
 }
-char Or(char c1,char c2){
-    if(c1=='V' || c2=='V'){
-        return 'V';
+
+int main(){
+    char c;
+    int loop = 1;
+    while((c = getchar()) != EOF){
+        if(op.size()>0) op.pop();
+        if(val.size()>0) val.pop();
+
+        do{
+            //printf("%c",c);
+            if(c == '('){
+            op.push(0);
+            }
+            else if(c == ')'){
+                while(!op.empty() && op.top()!=0) {
+                    calc();
+                }
+                op.pop();
+                insertVal(val.top(),1);
+            }
+
+            else if(c == '!'){
+                op.push(3);
+            }
+            else if(c == '&'){
+                while(!op.empty() && op.top()!=0 && op.top()>=2) {
+                    calc();
+                }
+                op.push(2);
+            }
+            else if(c == '|'){
+                while(!op.empty() && op.top()!=0 && op.top()>=1) {
+                    calc();
+                }
+                op.push(1);
+            }
+            else if(c == 'V'){
+                insertVal(1,0);
+            }
+            else if(c == 'F'){
+                insertVal(0,0);
+            }
+            s1 = val;
+            /*
+            for(int i = 0;i<s1.size();i++){
+                if(!s1.empty())  printf("%d",s1.top());
+                s1.pop();
+            }*/
+            //printf("\n");
+        }
+        while((c = getchar()) != '\n' && c!=EOF);
+
+        while(!op.empty()) calc();
+        /*
+        for(int i = 0;i<val.size();i++){
+            if(!val.empty())  printf("val: %d",val.top());
+            val.pop();
+        }*/
+        printf("Expression %d: %c\n", loop++, (val.top() ? 'V':'F'));
     }
-    return 'F';
-}
-char TorF(vector<char> v1){
-    while(v1.size()!=1){
-        for(int n=0;n<3;n++){
-            if(n==0){
-                for(int m=0;m<v1.size();m++){
-                    if(v1[m] == '!'){
-                        v1.erase(v1.begin()+m);
-                        v1.insert(v1.begin()+m, Not(m));
-                        v1.erase(v1.begin()+m+1);
-                    }
-                }
-            }
-            if(n==1){
-                for(int m=0;m<v1.size();m++){
-                    if(v1[m] == '&'){
-                        v1.erase(v1.begin()+m);
-                        v1.insert(v1.begin()+m, And(v1[m-1],v1[m]));
-                        v1.erase(v1.begin()+m+1);
-                        v1.erase(v1.begin()+m-1);
-                    }
-                }
-            }
-            if(n==2){
-                for(int m=0;m<v1.size();m++){
-                    if(v1[m] == '|'){
-                        v1.erase(v1.begin()+m);
-                        v1.insert(v1.begin()+m, Or(v1[m],v1[m-1]));
-                        v1.erase(v1.begin()+m+1);
-                        v1.erase(v1.begin()+m-1);
-                    }
-                }
-            }
-        }
-    }
-
-    return v1[0];
-}
-int main() {
-    int i,j,d=1;
-    char c,c1;
-    bool out=false;
-    string reg;
-
-    while(getline(cin,reg)){
-        for(int g=0;g<reg.size();g++){
-            if(reg[g]!=' ') v.push_back(reg[g]);
-        }
-        for(i=0;i<v.size();i++){
-            if(v[i]==')'){
-                v.erase(v.begin()+i);
-                for(j=i-1;v[j]!='(';j--){
-                    nv.insert(nv.begin(), v[j]);
-                    v.erase(v.begin()+j);
-                }
-                v.erase(v.begin()+j);
-                v.insert(v.begin()+j, TorF(nv));
-                nv.clear();
-                i=0;
-            }
-            if(i==v.size()-1){
-                out = true;
-            }
-        }
-        if(out){
-            cout<<"Expression"<<" "<<d<<":"<<" "<<TorF(v)<<"\n";
-            d++;
-            v.clear();
-        }
-    }
-
-
-
     return 0;
 }
